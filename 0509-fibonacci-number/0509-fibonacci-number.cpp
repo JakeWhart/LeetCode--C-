28320514c1fc0cf12378ec582876c714f6f8a54a
OM MAHAJAN <90970162+JakeWhart@users.noreply.github.com>
2026-01-01 16:32:19 +0530

    Time: 3 ms (62.2%), Space: 7.9 MB (36.12%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        int fib = 0;
+        int prev = 0;
+        int prev2 = 1;
+        for(int i = 0; i < n;i++){
+            fib = prev+prev2;
+            prev2 = prev;
+            prev = fib;
+        }
+        return fib;
+    }
+};
