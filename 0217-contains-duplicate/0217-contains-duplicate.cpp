ecd745b78a666ab64797d8bfcc240b76762e15fd
OM MAHAJAN <90970162+JakeWhart@users.noreply.github.com>
2026-01-08 16:44:31 +0530

    Time: 24 ms (84.06%), Space: 71.1 MB (79.4%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        bool contain = false;
+        sort(nums.begin(),nums.end());
+        for(int i = 1; i < nums.size();i++){
+            if(nums[i-1]==nums[i]) contain = true;
+        }
+   return contain; }
+};
